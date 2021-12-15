ce36f68d92fa7eb56b52f3fa9ab881b431196e66
Matthew E Newton <Elijah2895@hotmail.com>
2021-12-15 16:50:42 -0500

    Add files via upload

---
+++ Contact Service/contact/ContactService.java
@@ -0,0 +1,59 @@
+package contact;
+
+import java.util.ArrayList;
+
+public class ContactService {
+
+    private ArrayList<Contact> contacts;
+  
+    public ContactService() {
+ 
+    contacts = new ArrayList<>();
+    }
+    public boolean addContact(Contact contact){
+        boolean contactAlready= false;
+        
+        for (Contact contactList:contacts) {
+        
+            if (contactList.equals(contact)) {
+                contactAlready = true;
+            }
+        }
+            if (!contactAlready) {
+                contacts.add(contact);
+                return true;
+            } else {
+            return false;
+        }
+    }
+    public boolean deleteContact(String contactID) {
+        for (Contact contactList:contacts) {
+            if (contactList.getContactID().equals(contactID)) {          
+                contacts.remove(contactList);
+                    return true;
+            }
+        }
+        return false;
+    }
+      
+    public boolean updateContact(String contactID, String firstName, String lastName, String phoneNumber, String address) {
+    for (Contact contactList:contacts) {
+        if (contactList.getContactID().equals(contactID)) {
+            if(!firstName.equals("") && !(firstName.length()>10)) {
+                contactList.setFirstName(firstName);
+            }
+            if(!lastName.equals("") && !(lastName.length()>10)) {
+                contactList.setFirstName(lastName);
+            }
+            if(!phoneNumber.equals("") && (phoneNumber.length()==10)) {
+                contactList.setFirstName(phoneNumber);
+            }
+            if(!address.equals("") && !(address.length()>30)) {
+                contactList.setFirstName(address);
+            }
+            return true;
+        }
+    }
+    return false;
+}
+}
---
+++ Contact Service/contact/ContactServiceTest.java
@@ -0,0 +1,49 @@
+package contact;
+public class ContactServiceTest {
+    
+   @Test
+
+   public void testAdd() {
+        ContactService cs = new ContactService();
+        Contact test1 = new Contact("1413252", "Jane", "Doe", "4444444444", "Sample 24 Drive");
+        assertEquals(true, cs.addContact(test1));
+   }
+
+   private void assertEquals(boolean b, Object addContact) {
+}
+
+@Test
+    
+   public void testDelete() {
+        ContactService cs = new ContactService();
+     
+        Contact test1 = new Contact("1413252", "Jane", "Doe", "4444444444", "Sample 24 Drive");
+        Contact test2 = new Contact("1309403", "Malleus", "Draconia", "2187123404", "Valley of Thorns");
+        Contact test3 = new Contact("9752319", "Vil", "Schoenheit", "9215501793", "Land of Proxynee");
+
+        cs.addContact(test1);
+        cs.addContact(test2);
+        cs.addContact(test3);
+
+        assertEquals(true, cs.deleteContact("1309403"));
+        assertEquals(false, cs.deleteContact("1309404"));
+        assertEquals(false, cs.deleteContact("1309403"));
+    }
+
+    @Test
+
+    public void testUpdate() {
+        ContactService cs = new ContactService();
+     
+        Contact test1 = new Contact("1413252", "Jane", "Doe", "4444444444", "Sample 24 Drive");
+        Contact test2 = new Contact("1309403", "Malleus", "Draconia", "2187123404", "Valley of Thorns");
+        Contact test3 = new Contact("9752319", "Vil", "Schoenheit", "9215501793", "Land of Proxynee");
+
+        cs.addContact(test1);
+        cs.addContact(test2);
+        cs.addContact(test3);
+
+        assertEquals(true, cs.updateContact("9752319", "VilFirst", "SchoenheitLast", "9215501793", "Land of Proxynee"));
+        assertEquals(false, cs.updateContact("9752322", "VilFirst", "SchoenheitLast", "9215501793", "Land of Proxynee"));
+    }
+}
